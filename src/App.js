4f18f666be83a376ab056a1eec02aef59c408c51
youjie <jennygril0313@gmail.com>
2024-05-13 23:27:38 +0800

    123

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MySlider from "./components/MySlider";
 function App() {
   return (
     <div className="App">
-      <h1>5B0G0029，陳又緁 第二元件</h1>
+      <h1>5B0G0029，陳又緁</h1>
       <MySlider />
       <h1>MyRGBPanel</h1>
       <MyRGBPanel />
