951d0c2384ac0521e850707d0c726db17d7971a3
youjie <jennygril0313@gmail.com>
2024-06-13 23:06:56 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MySlider from "./MySlider";
+import MyRGBPanel from "./components/MyRGBPanel";
+import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
-      <h1>Javascript程式設計，使用react</h1>
-      <h2>第一個元件</h2>
-      <span>R: </span><MySlider />
-      <span>G: </span><MySlider />
-      <span>B: </span><MySlider />
-      {/* <header className="App-header"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React! 2024.5.1</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-      {/* </header> */}
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
     </div>
   );
 }
